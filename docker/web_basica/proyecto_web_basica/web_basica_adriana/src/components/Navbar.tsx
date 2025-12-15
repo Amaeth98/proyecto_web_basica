d09d392a9b57f1d09fe60686eb6e004bd1311ffa
Amaeth <sniukis98@gmail.com>
2025-12-15 19:36:54 +0100

    cambio

--- docker/web_basica/proyecto_web_basica/web_basica_adriana/src/components/Navbar.tsx
+++ docker/web_basica/proyecto_web_basica/web_basica_adriana/src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function NavbarComponent() {
           <NavLink to="/fotos" className="nav-link">
             <span className="nav-icon-text">
               <FaScrewdriverWrench className="nav-icon" />
-              Foto
+              Fotografias
             </span>
           </NavLink>
 
